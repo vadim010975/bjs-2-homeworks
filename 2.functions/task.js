3a9b28d52a0b76ee3d4702b4077d40509e478abe
vadim010975 <vadim010975@mail.ru>
2023-05-14 21:53:20 +0300

    Выполнено первое задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,25 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  if (!arr.length) {
+    return;
+  }
+  let notNumber = false;
+  arr.forEach(function(value) {
+    if (!Number.isFinite(value)){
+      notNumber = true;
+    }
+  });
+  if (notNumber) {
+    return;
+  }
+  const minInArray = Math.min(...arr);
+  const maxInArray = Math.max(...arr);
+  const initialValue = 0;
+  const sumElements = arr.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+  );
+  const averageValue = parseFloat((sumElements / arr.length).toFixed(2));
+  return { min: minInArray, max: maxInArray, avg: averageValue };
 }
 
 function summElementsWorker(...arr) {
