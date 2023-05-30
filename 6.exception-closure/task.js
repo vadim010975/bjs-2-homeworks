a59f9e76bb1281b5288e46edc966e1e4a47a5a58
vadim010975 <vadim010975@mail.ru>
2023-05-30 21:46:34 +0300

    часть 6 в процессе

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,21 @@
-﻿
+﻿function parseCount(value) {
+	const parsedValue = Number.parseFloat(value);
+	if (Number.isNaN(parsedValue)) {
+		throw new Error("Невалидное значение");
+	}
+	return parsedValue;
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(sideA, sideB, sideC) {
+		
+	}
+}
