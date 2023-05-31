a212407624cd005458125374ee954e121cce2763
vadim010975work <vadim010975@mail.ru>
2023-05-31 14:58:09 +0300

    part 6 is completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,7 +1,7 @@
 ﻿function parseCount(value) {
 	const parsedValue = Number.parseFloat(value);
 	if (Number.isNaN(parsedValue)) {
-		throw new Error("Невалидное значение");
+		throw new Error('Невалидное значение');
 	}
 	return parsedValue;
 }
@@ -16,6 +16,26 @@ function validateCount(value) {
 
 class Triangle {
 	constructor(sideA, sideB, sideC) {
-		
+		if (sideA + sideB < sideC || sideB + sideC < sideA || sideA + sideC < sideB) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+    this.sideA = sideA;
+    this.sideB = sideB;
+    this.sideC = sideC;
 	}
+  get perimeter() { return this.sideA + this.sideB + this.sideC }
+  get area() {
+    return parseFloat(((Math.sqrt(4 * this.sideA**2 * this.sideB**2 - (this.sideA**2 + this.sideB**2 - this.sideC**2)**2)) / 4).toFixed(3));
+  }
+}
+
+function getTriangle (sideA, sideB, sideC) {
+  try {
+    return new Triangle(sideA, sideB, sideC);
+  } catch {
+    return {
+      get perimeter() { return 'Ошибка! Треугольник не существует'},
+      get area() { return 'Ошибка! Треугольник не существует'}
+    }
+  }
 }
